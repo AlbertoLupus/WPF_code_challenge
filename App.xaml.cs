8aa66087e92bb5ada90712fd0c6e43c0346fdb62
Albert Kirner <albert.kirner@freenet.de>
2024-01-25 10:08:14 +0100

    initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace wscad_coding_challenge;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    public App()
+    {
+    }        
+}
+
